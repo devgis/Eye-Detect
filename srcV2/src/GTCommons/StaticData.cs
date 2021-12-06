1f67e574febbe00c4be5882ff9a4414a2e41d9e5
devgis <devgis@163.com>
2021-12-06 21:14:33 +0800

    Update some code
    Update some code

--- srcV2/src/GTCommons/StaticData.cs
+++ srcV2/src/GTCommons/StaticData.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 
 namespace GTCommons
 {
+    /// <summary>
+    /// liyafei add save the data
+    /// </summary>
     public class StaticData
     {
         public static bool IsWhitePointEnabled = false;
@@ -14,6 +18,9 @@ namespace GTCommons
         public static MyPosition WhitePointPosition = new MyPosition(0,0);
         public static MyPosition BlackCrossPosition = new MyPosition(0, 0);
         public static MyPosition WhiteCrossPosition = new MyPosition(0, 0);
+        static string filePath = Path.Combine(Environment.CurrentDirectory, "positions.txt");
+        static string lastMessage = string.Empty;
+        static bool WriteFile = true;
 
         public static string Message
         {
@@ -35,6 +42,15 @@ namespace GTCommons
                 if (string.IsNullOrEmpty(message.Trim()))
                 {
                     message = "Not Found";
+                }
+                else
+                {
+                    if (WriteFile&&message != lastMessage)
+                    {
+                        //写文件
+                        File.AppendAllText(filePath, $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}] {message}\r\n", Encoding.Default);
+                        lastMessage = message;
+                    }
                 }
                 return message;
             }
